ecf8cd3355f91e12b8c052ce3a65b51ea93a37b2
Alexandre Diniz Veloso <alexandredinizveloso@gmail.com>
2022-12-12 20:13:42 -0300

    Create main.py

---
+++ 3_hebb_rule/main.py
@@ -0,0 +1,26 @@
+entry = []
+archive = open('num.txt', 'r')
+
+for line in archive:
+    vector_entry = line.split(',')
+    entry.append(vector_entry)
+
+w = []
+
+for i in range(len(entrys[0]) - 1):
+    w.append(0)
+
+outp = []
+
+for i in range(len(entry)):
+    outp.append(entry[i][len(entry[i]) - 1])
+
+for i in range(len(entry)):
+    x = []
+    
+    for j in range(len(entry[i]) - 1):
+        x.append(entry[i][j])
+        y = outp[i]
+        w[j] = w[j] + (int(x[j]) * int(y))
+
+    print('\nAs entradas são: ', x, 'As saidas são: ', saidas[i], 'Os pesos são', pesos)
