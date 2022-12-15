8ce30c4fa0bf2fc0841f863b5584991b265621b1
Alexandre Diniz Veloso <alexandredinizveloso@gmail.com>
2022-12-15 13:44:40 -0300

    Update main.py

--- 4_perceptron/main.py
+++ 4_perceptron/main.py
@@ -1,56 +1,56 @@
-train = []
-archive = open('num.txt', 'r')
+treino_inicial = []
+arquivo = open('num.txt', 'r')
 
-for line in archive:
-    vector_num = line.split(',')
-    train.append(vector_num)
+for linhas in arquivo:
+    vetor_numeros = linhas.split(',')
+    treino_inicial.append(vetor_numeros)
 
-w = []
+pesos = []
 
-for i in range(len(train[0])-1):
-    w.append(0)
+for i in range(len(treino_inicial[0])-1):
+    pesos.append(0)
 
-exp_outp = []
 
-for i in range(len(train)):
-    exp_outp.append(train[i][len(train[i])-1])
+saida_esperada = []
 
-w = [0,0,0]
-a = 1
-t = 0
-test = 0
-epoch = 0
-training = True
+for i in range(len(treino_inicial)):
+    saida_esperada.append(treino_inicial[i][len(treino_inicial[i])-1])
 
-while(training):
-        epoch += 1
-        test = 0
-      
-        for i in range(len(train)):
-            summ = 0
-            summ += int(w[2])
-            
-            for j in range(len(train[i])-1):
-                summ += int(train[i][j]) * int(w[j])
+pesos = [0,0,0]
+alfa = 1
+teta = 0
+teste =0
+epocas = 0
 
-            if(summ > t):
+treinamento = True
+
+while(treinamento):
+        epocas+=1
+        teste = 0
+        for i in range(len(treino_inicial)):
+            somatorio = 0
+            somatorio += int(pesos[2])
+            for j in range(len(treino_inicial[i])-1):
+                somatorio += int(treino_inicial[i][j]) * int(pesos[j])
+
+            if(somatorio > teta):
                 y = 1
-            elif((summ >= -t) and (summ <= t)):
+            elif((somatorio>=-teta)and(somatorio<=teta)):
                 y = 0
-            elif(summ < -t):
+            elif(somatorio < -teta):
                 y = -1
 
-            if(int(exp_outp[i]) == y):
-                test += 1
-                if test == 1:
-                    training = False
+            if(int(saida_esperada[i]) == y):
+                teste += 1
+                if teste == 1:
+                    treinamento = False
             else:
-                leng = len(w)
-                for j in range(leng):
+                tamanho = len(pesos)
+                for j in range(tamanho):
                     if j == 2:
-                        w[j] = int(w[j]) + (a * int(exp_outp[i]))
+                        pesos[j] = int(pesos[j])+(alfa*int(saida_esperada[i]))
                     else:
-                        w[j] = int(w[j]) + (a * int(exp_outp[i]) * int(train[i][j]))
-            print('\nAs entradas são: ',train[i],'As saídas são: ',exp_output[i],'Os pesos são: ',w)
-        epoch += 1
-        print('Número de epocas é igual a: '+str(epoch))
+                        pesos[j] = int(pesos[j]) + (alfa*int(saida_esperada[i])*int(treino_inicial[i][j]))
+            print('\nEntradas=',treino_inicial[i],'Saida=',saida_esperada[i],'Pesos=',pesos)
+        epocas += 1
+        print('Epocas = '+str(epocas))
